531e4cd4cf83a47318a04d9d8268281a861df273
Vivatey <vivatey@gmail.com>
2023-04-14 01:50:51 +0100

    Created HTML foundation or skeleton for project - search form, App border, placeholder city and weather information, in ideal alignment using rows.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         Coded by Viv and is{" "}
         <a
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-dark" />
+          </div>
+        </div>
+      </form>
+      <h1>London</h1>
+      <ul>
+        <li>Friday 01:00</li>
+        <li>Clear with periodic clouds</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="clear"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 79%</li>
+            <li>Wind: 3 mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
