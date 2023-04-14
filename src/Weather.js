3b719da8d67a11b8bf49e2971c6911a34e9bac35
Vivatey <vivatey@gmail.com>
2023-04-14 05:16:08 +0100

    Styled the Weather component  - using d-flex for positioning the image, temperature and unit; vanilla css for everything else.

--- src/Weather.js
+++ src/Weather.js
@@ -11,25 +11,36 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-dark" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-dark w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>London</h1>
-      <ul>
-        <li>Friday 01:00</li>
-        <li>Clear with periodic clouds</li>
-      </ul>
-      <div className="row">
+      <div className="city">
+        <h1>London</h1>
+        <ul>
+          <li>Friday 01:00</li>
+          <li>Clear with periodic clouds</li>
+        </ul>
+      </div>
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
-            alt="clear"
-          />
-          6°C
+          <div className="d-flex">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+              alt="clear"
+            />
+
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
